9003f7470474a3c4adedb78e3f27ef8913f22eef
Amirhossein <ahbaghero01@gmail.com>
2023-01-06 19:40:54 +0330

    revised query 1

--- phase2/procedure1.sql
+++ phase2/procedure1.sql
@@ -3,7 +3,9 @@ USE dbproject;
 DROP PROCEDURE IF EXISTS participate_survey;
 CREATE PROCEDURE participate_survey (IN passnum int, IN ticketnum int,OUT questions_id int)
        BEGIN
-            SELECT qu.id INTO questions_id FROM dbproject.ticket as tk INNER JOIN dbproject.question as qu on tk.surveyid = qu.surveyid INNER JOIN survey as sr on sr.id = tk.surveyid where sr.active = true;
+            SELECT qu.id INTO questions_id FROM dbproject.ticket as tk INNER JOIN dbproject.question as qu on tk.surveyid = qu.surveyid INNER JOIN survey as sr on sr.id = tk.surveyid 
+            where sr.active = true AND tk.ticket_typeid = qu.ticket_typeid 
+            AND EXISTS (SELECT ap.questionid FROM dbproject.approving as ap where ap.questionid = qu.id);
        END;
 
 CALL participate_survey(1,2, @questionids);
