18e8ed5940b1279312adb9708774389802af5c59
Amirhossein <ahbaghero01@gmail.com>
2023-01-08 17:02:16 +0330

    added P4

--- phase2/procedure4.sql
+++ phase2/procedure4.sql
@@ -1,23 +1,12 @@
 USE dbproject;
-DROP PROCEDURE IF EXISTS qdmanageraccesss;
-CREATE PROCEDURE qdmanageraccesss (IN mng int)
-       BEGIN
-            SELECT * FROM q_descriptive as q 
-            where q.questionid in (SELECT question.id from question
-            EXCEPT
-            SELECT approving.questionid from approving);
-       END;
-
-CALL qdmanageraccesss();
 
 DROP PROCEDURE IF EXISTS qmmanageraccesss;
 CREATE PROCEDURE qmmanageraccesss (IN mng int)
        BEGIN
-            SELECT * FROM q_multiple_choice as q INNER JOIN options as op on op.questionid = q.questionid
-            where q.questionid in (SELECT question.id from question
-            EXCEPT
-            SELECT approving.questionid from approving)
-            ORDER BY q.questionid ASC , op.number ASC;
+          SET @COUNTNUM = 1;
+          SELECT COUNT(DISTINCT t.ticket_number) INTO @COUNTNUM FROM ticket as t LEFT JOIN survey as sr on sr.id = t.surveyid LEFT JOIN question as q on q.surveyid = sr.id LEFT JOIN ticket_type as tkt on tkt.typeid = t.ticket_typeid where (1 in (SELECT air.managerID FROM ticket as t2 INNER JOIN flight as f INNER JOIN airline as air where t.ticket_number = t2.ticket_number)) OR (mng in (SELECT as2.assistanceid FROM ticket as t2 INNER JOIN flight as f INNER JOIN airline as air INNER JOIN assistance as as2 on as2.adminid = air.managerID where t.ticket_number = t2.ticket_number));
+          SELECT q.id,tkt.type_string,COUNT(*)/@COUNTNUM FROM ticket as t LEFT JOIN survey as sr on sr.id = t.surveyid LEFT JOIN question as q on q.surveyid = sr.id LEFT JOIN ticket_type as tkt on tkt.typeid = t.ticket_typeid where ((1 in (SELECT air.managerID FROM ticket as t2 INNER JOIN flight as f INNER JOIN airline as air where t.ticket_number = t2.ticket_number)) OR (mng in (SELECT as2.assistanceid FROM ticket as t2 INNER JOIN flight as f INNER JOIN airline as air INNER JOIN assistance as as2 on as2.adminid = air.managerID where t.ticket_number = t2.ticket_number))) AND q.id IS NOT NULL
+          GROUP BY q.id,tkt.type_string;
        END;
 
-CALL qmmanageraccesss();
+CALL qmmanageraccesss(1);
